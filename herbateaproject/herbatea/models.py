bf21f46aa9f1385d185dbebb43c6adede9bd401a
Texas Brown <200074021@aston.ac.uk/Users/tiygatex/Desktop/herbatea>
2022-11-28 00:42:07 +0000

    Created the 'Contact Us' database

--- herbateaproject/herbatea/models.py
+++ herbateaproject/herbatea/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class ContactUs(models.Model): 
+   iD = models.IntegerField(primary_key = True)
+   full_name = models.CharField(max_length = 50)
+   email = models.EmailField(blank = False, null = False)
+   mobile_number = models.CharField(max_length = 20, blank = True)
+   description = models.CharField(max_length = 500)
+
+
+   def __str__(self):
+      return self.full_name + '' + self.email
