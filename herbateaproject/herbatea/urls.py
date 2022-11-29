8b6a0bce892b0aa141804e8c01573eb52f9336c5
Texas Brown <200074021@aston.ac.uk/Users/tiygatex/Desktop/herbatea>
2022-11-29 00:22:06 +0000

    Combined 'urls.py' and placed in 'herbatea' folder

--- herbateaproject/herbatea/urls.py
+++ herbateaproject/herbatea/urls.py
@@ -1,6 +1,15 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 from . import views
+from herbatea.views import signin, signup, trackorder, orders
+
 
 urlpatterns = [
    path('', views.home, name="home"),
+   path('admin/', admin.site.urls, name = "admin"),
+   path('', include('herbatea.urls')),
+   path('signin/', signin, name = "sign_in"),
+   path('signup/', signup, name = "sign_up"),
+   path('trackorder/', trackorder, name = "track_order"),
+   path('orders/', views.orders, name = "orders"),
 ]
