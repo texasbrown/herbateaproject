5f3bbf958eca871ce46eb08e344bfe8478fa3e33
yasinv <210034354@aston.ac.uk>
2022-11-29 12:35:03 +0000

    updated urls and views to access html pages

--- herbateaproject/store/urls.py
+++ herbateaproject/store/urls.py
@@ -4,8 +4,11 @@ from django.urls import path
 
 urlpatterns = [
    path('', views.home, name="home"),
+   path('about/', views.about, name='about'),
    path('signin/', views.signin, name = "sign_in"),
    path('signup/', views.signup, name = "sign_up"),
    path('trackorder/', views.trackorder, name = "track_order"),
    path('orders/', views.orders, name = "orders"),
+   path('contact/', views.contact, name="contact"),
+	path('thankyou/', views.thankyou, name="thankyou"),
 ]
--- herbateaproject/store/views.py
+++ herbateaproject/store/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from .models import Orders
+from .models import *
 
 # Create your views here.
 def home(request):
     return render(request, 'herbatea/home.html ', {})
+
+def about(request):
+    return render(request, 'store/about.html', {'title': 'About'})
 
 def signin(request):
     return render(request, 'herbatea/signin.html', {} )
@@ -18,3 +21,11 @@ def orders(request):
     orders_list = Orders.objects.all()
     return render(request, 'herbatea/orders.html', 
     { 'orders': orders_list })
+
+def contact(request):
+    context = {}
+    return render(request, 'store/contact.html', context)
+
+def thankyou(request):
+    context = {}
+    return render(request, 'store/thankyou.html', context)
