3a4a070281e6220917309592145ce6af147323c8
Texas Brown <200074021@aston.ac.uk/Users/tiygatex/Desktop/herbatea>
2022-11-29 11:13:28 +0000

    Updated models

--- herbateaproject/herbatea/models.py
+++ herbateaproject/herbatea/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 
+
 class ContactUs(models.Model): 
    iD = models.IntegerField(primary_key = True)
    full_name = models.CharField(max_length = 50)
@@ -14,29 +15,32 @@ class ContactUs(models.Model):
 
 
 class Customer(models.Model):
-   customerid= models.IntegerField(primary_key = True)
-   fname = models.CharField(max_length=50)
-   lname = models.CharField(max_length=100)
-   email = models.EmailField(max_length=200)
-   password = models.CharField (max_length=50)
+   customerid = models.IntegerField(primary_key=True, default='1', blank=True)
+   fname = models.CharField(max_length=50, null = True)
+   lname = models.CharField(max_length=100, null = True)
+   email = models.EmailField(max_length=200, null = True)
+   password = models.CharField(max_length=50, null = True)
 
    def __str__(self):
       return self.fname + ' ' + self.lname
 
 
+class Products(models.Model):
+   pass
+
 class Orders(models.Model):
-   customerID = models.ForeignKey('Customer Account ID', on_delete=models.PROTECT)
-   orderID = models.CharField(primary_key=True)
-   productID = models.ForeignKey('Prouct List', on_delete=models.PROTECT)
-   email = models.ForeignKey('Customer Email', on_delete=models.PROTECT)
-   delivery_address = models.CharField('Customer Delivery Address')
-   total_price = models.FloatField('Total Price of Combined orders')
-   # Update the "number_of_orders" section to draw from users table.
-   orders_placed = models.ManyToMany('Customers Table', on_delete=models.PROTECT)
-   order_date = models.DateTimeField('Date and time order was placed')
-   order_status = models.CharField('Status would either be "Placed" or "Declined"', max_length=10)
+   customerID = models.ForeignKey(Customer, null = True, on_delete=models.PROTECT)
+   orderID = models.CharField(primary_key = True, max_length = 50)
+   productID = models.ForeignKey('Products', on_delete=models.PROTECT)
+   email = models.EmailField(null = True)
+   delivery_address = models.CharField(max_length = 300, null = True)
+   total_price = models.FloatField(null = True)
+   # Many users can make many orders.
+   #orders = models.ManyToMany('Self', Customer, blank = True, on_delete=models.PROTECT)
+   order_date = models.DateTimeField(auto_now = True, null = True)
+   order_status = models.CharField(max_length = 10, null = True)
 
    # change 'self.customerID' to field in table to show the name of the customer and for "ProductID" to shoew the product name
 
-   def __str__(self):
-      return self.orderID + '' + self.order_date + '' + self.customerID + '' + self.email + '' + self.productID + '' + self.total_price
+#def __str__(self):
+#     return self.orderID + '' + self.order_date + '' + self.customerID.fname + '' + self.email
