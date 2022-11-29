80be6943071af5156d69c03cba341a205db8f25a
Texas Brown <200074021@aston.ac.uk/Users/tiygatex/Desktop/herbatea>
2022-11-29 21:36:55 +0000

    added contactus table and made migrations

--- HerbaTea/store/models.py
+++ HerbaTea/store/models.py
@@ -72,3 +72,12 @@ class ShippingAddress(models.Model):
 	def __str__(self):
 		return self.address
 	
+
+class Contact(models.Model):
+   full_name = models.CharField(max_length=50)
+   email = models.EmailField(blank=False, null=False)
+   mobile_number = models.CharField(max_length=20, blank=True)
+   description = models.CharField(max_length=500)
+
+   def __str__(self):
+      return self.full_name + '' + self.email
