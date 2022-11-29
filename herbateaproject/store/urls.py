4f72dce4a64e37dba8c6d523e2036389dcbc24a7
Texas Brown <200074021@aston.ac.uk/Users/tiygatex/Desktop/herbatea>
2022-11-29 13:49:27 +0000

    added store back to repo

---
+++ herbateaproject/store/urls.py
@@ -0,0 +1,16 @@
+from . import views
+from django.urls import path
+
+
+urlpatterns = [
+   path('', views.home, name="home"),
+   path('about/', views.about, name='about'),
+   path('signin/', views.signin, name = "sign_in"),
+   path('signup/', views.signup, name = "sign_up"),
+   path('trackorder/', views.trackorder, name = "track_order"),
+   path('orders/', views.orders, name = "orders"),
+   path('contact/', views.contact, name="contact"),
+	path('thankyou/', views.thankyou, name="thankyou"),
+   path('cart/', views.cart, name="cart"),
+	path('checkout/', views.checkout, name="checkout"),
+]
