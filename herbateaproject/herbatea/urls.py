7e35721d85ccfa734c8c12d1962eab41d639fc0b
Texas Brown <200074021@aston.ac.uk/Users/tiygatex/Desktop/herbatea>
2022-11-29 01:51:19 +0000

    removed additional spacing

--- herbateaproject/herbatea/urls.py
+++ herbateaproject/herbatea/urls.py
@@ -2,10 +2,8 @@ from . import views
 from django.urls import path
 
 
-
 urlpatterns = [
    path('', views.home, name="home"),
-
    path('signin/', views.signin, name = "sign_in"),
    path('signup/', views.signup, name = "sign_up"),
    path('trackorder/', views.trackorder, name = "track_order"),
